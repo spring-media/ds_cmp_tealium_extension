267639c2f873a98341755f9b95107522de3f770c
Nicole Emer <nicole.emer@axelspringer.com>
2024-01-31 10:33:41 +0100

    changed twitter setup, we now use regex to exclude getpocket referrer

--- extensions/doPlugins_global.js
+++ extensions/doPlugins_global.js
@@ -599,7 +599,7 @@ s._setExternalReferringDomainEvents = function (s) {
             channelCategory: 'Youtube',
         },
         {
-            domains: ['t.co', 'twitter.com', 'android-app://com.twitter.android'],
+            matchRegex:  /^t.co|twitter.com|android-app:\/\/com.twitter.android/,
             event: 'event51,event222',
             channel: 'Organic Social',
             channelCategory: 'Twitter',
