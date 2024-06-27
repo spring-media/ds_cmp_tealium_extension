a071fac61d6581e3c6527f7dfc74f448e4c2d61c
Mehak Raza <mehakraza258@gmail.com>
2024-06-27 13:47:41 +0200

    Added unit tests for brandstory milestones

---
+++ tests/brandstory/brandstory_milestones.test.js
@@ -0,0 +1,78 @@
+const brandstory_milestones = require('../../extensions/brandstory/brandstory_milestones');
+
+describe('getDomainTagValue', () => {
+    it('should return [206] for domain containing "welt.de"', () => {
+        expect(brandstory_milestones.getDomainTagValue('subdomain.welt.de')).toEqual([206]);
+    });
+
+    it('should return [10] for domain containing "bild.de"', () => {
+        expect(brandstory_milestones.getDomainTagValue('subdomain.bild.de')).toEqual([10]);
+    });
+
+    it('should return [] for a domain that does not match', () => {
+        expect(brandstory_milestones.getDomainTagValue('example.com')).toEqual([]);
+    });
+});
+
+// Mocking window object
+describe('setMilestones', () => {
+    let originalWindow;
+
+    beforeAll(() => {
+        originalWindow = { ...window };
+    });
+
+    beforeEach(() => {
+        // Reset window object before each test
+        window.location.hostname = '';
+        window.utag = {
+            link: jest.fn(),
+            data: {
+                adobe_pageName: 'testPageName',
+                page_escenicId: 'testEscenicId',
+                page_platform: 'testPlatform',
+                page_type: 'testType',
+                page_sectionPath: 'testSectionPath'
+            }
+        };
+    });
+
+    afterAll(() => {
+        // Restore original window object
+        window = originalWindow;
+    });
+
+    it('should set milestones with the correct parameters', () => {
+        window.location.hostname = 'subdomain.welt.de';
+        const tagNumber = brandstory_milestones.getDomainTagValue(window.location.hostname);
+
+        // Use fake timers
+        jest.useFakeTimers(); 
+
+        window.onload();
+
+        const milestones = [
+            { label: '5', time: 5000 },
+            { label: '30', time: 30000 },
+            { label: '60', time: 60000 },
+            { label: '180', time: 180000 }
+        ];
+
+        milestones.forEach(milestone => {
+            jest.advanceTimersByTime(milestone.time);
+
+            expect(window.utag.link).toHaveBeenCalledWith({
+                event_name: 'article_milestone',
+                event_label: milestone.label,
+                adobe_pageName: 'testPageName',
+                page_escenicId: 'testEscenicId',
+                page_platform: 'testPlatform',
+                page_type: 'testType',
+                page_sectionPath: 'testSectionPath'
+            }, null, tagNumber);
+        });
+
+        // Restore real timers
+        jest.useRealTimers(); 
+    });
+});
