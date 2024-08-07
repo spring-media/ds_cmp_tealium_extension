6c676d2dd9d72085b7252e74d6824af72b2802b5
Nicole Emer <nicole.emer@axelspringer.com>
2024-08-07 09:25:24 +0200

    user_hasPurSubscription is in b object needed for Tealium Loadrules

--- extensions/whoami.js
+++ extensions/whoami.js
@@ -62,8 +62,8 @@ _whoamiSnipped_log('whoami : start  ');
     var isLoggedIn = asinfo !== null && asinfo.subOrigin === 'as';
     window.utag.data.user_isLoggedIn2 = isLoggedIn === true ? 'true' : 'false';
     if (window.utag.data.user_isLoggedIn2 === 'false') {
-        window.utag.data.user_hasPurSubscription2 = 'false';
-        b.user_hasPurSubscription2 = window.utag.data.user_hasPurSubscription2;
+        window.utag.data.user_hasPurSubscription = 'false';
+        b.user_hasPurSubscription = window.utag.data.user_hasPurSubscription;
         window.utag.data.user_hasPlusSubscription2 = 'false';
         window.utag.data.user_jaId2 = 'false';
         window.utag.data.user_entitlements2 = 'false';
