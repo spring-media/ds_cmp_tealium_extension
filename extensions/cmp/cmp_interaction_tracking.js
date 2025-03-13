42cf760128d4ba8fb2614de230d27c425effd034
Nicole Emer <nicole.emer@axelspringer.com>
2025-03-13 13:11:44 +0100

    back to older version how we set cmp_event_status, correction of mistake how we set cmp_event_status

--- extensions/cmp/cmp_interaction_tracking.js
+++ extensions/cmp/cmp_interaction_tracking.js
@@ -197,7 +197,7 @@
     }
 
     function onCmpuishown(tcData) {
-        window.utag.data.cmp_event_status = !window.utag.data.cmp_event_status ? tcData.eventStatus : window.utag.data.cmp_event_status;
+        window.utag.data.cmp_event_status = tcData.eventStatus;
         if (tcData && tcData.eventStatus === 'cmpuishown') {
             window.utag.data.cmp_events = TCFAPI_COMMON_EVENTS.CMP_UI_SHOWN;
             exportedFunctions.sendFirstPageViewEvent();
