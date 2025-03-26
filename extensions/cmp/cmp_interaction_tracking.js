895f906336514aa7ac3c5a77da3d189097806053
Nicole Emer <nicole.emer@axelspringer.com>
2025-03-26 15:22:42 +0100

    new setup to get true/false and not '' in hasUserGivenConsent

--- extensions/cmp/cmp_interaction_tracking.js
+++ extensions/cmp/cmp_interaction_tracking.js
@@ -128,7 +128,8 @@
     }
 
     function hasUserDeclinedConsent() {
-        const hasUserGivenConsent = window.utag.data.consentedVendors && window.utag.data.consentedVendors != '' && window.utag.data.consentedVendors.includes('adobe_analytics');
+        const consentedVendors = window.utag.data.consentedVendors || '';
+        const hasUserGivenConsent =  consentedVendors.includes('adobe_analytics') ? true : false;
         const isAfterCMP = exportedFunctions.isAfterCMP();
 
         return hasUserGivenConsent ? false : isAfterCMP;
