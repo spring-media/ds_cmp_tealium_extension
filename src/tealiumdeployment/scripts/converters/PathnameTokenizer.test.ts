10a1d85ccdaf001a4b865ae402800e7ece2460fd
Tobias Steuer <tobias.steuer@axelspringer.com>
2026-02-12 18:01:57 +0100

    [TIPPI-1332] Add PathnameTokenizer

---
+++ src/tealiumdeployment/scripts/converters/PathnameTokenizer.test.ts
@@ -0,0 +1,30 @@
+import { PathnameTokenizer } from './PathnameTokenizer';
+import { ExtensionData } from './types';
+
+describe('PathnameTokenizer', ()=> {
+    it('create extension', ()=>{
+        const converter = new PathnameTokenizer();
+        const extension: ExtensionData = {
+            name: 'test name',
+            id: 123,
+            scope: '',
+            extensionType: '',
+            occurrence: null,
+            loadRule: null,
+            conditions: [],
+            configuration: {}
+        } 
+        const code = `/* eslint-disable */\n`
+        + `/* Based on PATHNAME TOKENIZER test name 123 */\n`
+        + `(function(a, b, c) {\n`
+        + `    if (typeof utag_data == 'undefined')\n`
+        + `        utag_data = {};\n`
+        + `    a = location.pathname.split('/');\n`
+        + `    b = (a.length > 9) ? 9 : a.length;\n`
+        + `    for (c = 1; c < b; c++) {\n`
+        + `        utag_data['_pathname' + c] = (typeof a[c] != 'undefined') ? a[c] : ''\n`
+        + `    }\n`
+        + `})();\n`;
+        expect(converter.convert(extension)).toBe(code);
+    })
+});
---
+++ src/tealiumdeployment/scripts/converters/PathnameTokenizer.ts
@@ -0,0 +1,18 @@
+import { Converter, ExtensionData } from './types';
+
+export class PathnameTokenizer implements Converter {
+    convert(extension: ExtensionData): string | false {
+        const code = '/* eslint-disable */\n'
+        + `/* Based on PATHNAME TOKENIZER ${extension.name} ${extension.id} */\n`        
+        + `(function(a, b, c) {\n`
+        + `    if (typeof utag_data == 'undefined')\n`
+        + `        utag_data = {};\n`
+        + `    a = location.pathname.split('/');\n`
+        + `    b = (a.length > 9) ? 9 : a.length;\n`
+        + `    for (c = 1; c < b; c++) {\n`
+        + `        utag_data['_pathname' + c] = (typeof a[c] != 'undefined') ? a[c] : ''\n`
+        + `    }\n`
+        + `})();\n`;
+        return code;
+    }
+}
