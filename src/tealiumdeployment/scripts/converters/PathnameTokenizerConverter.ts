40013c5ed20040663ed83184133060366d1cc96d
Tobias Steuer <tobias.steuer@axelspringer.com>
2026-02-16 15:39:59 +0100

    [TIPPI-1332] Added try and catch block

--- src/tealiumdeployment/scripts/converters/PathnameTokenizerConverter.ts
+++ src/tealiumdeployment/scripts/converters/PathnameTokenizerConverter.ts
@@ -5,12 +5,16 @@ export class PathnameTokenizerConverter implements Converter {
         const code = '/* eslint-disable */\n'
         + `/* Based on PATHNAME TOKENIZER ${extension.name} ${extension.id} */\n`        
         + `(function(a, b, c) {\n`
-        + `    if (typeof utag_data == 'undefined')\n`
-        + `        utag_data = {};\n`
-        + `    a = location.pathname.split('/');\n`
-        + `    b = (a.length > 9) ? 9 : a.length;\n`
-        + `    for (c = 1; c < b; c++) {\n`
-        + `        utag_data['_pathname' + c] = (typeof a[c] != 'undefined') ? a[c] : ''\n`
+        + `    try {\n`
+        + `        if (typeof utag_data == 'undefined')\n`
+        + `            utag_data = {};\n`
+        + `        a = location.pathname.split('/');\n`
+        + `        b = (a.length > 9) ? 9 : a.length;\n`
+        + `        for (c = 1; c < b; c++) {\n`
+        + `            utag_data['_pathname' + c] = (typeof a[c] != 'undefined') ? a[c] : ''\n`
+        + `        }\n`
+        + `    } catch(e) {\n`
+        + `        window.utag.DB(e);\n`
         + `    }\n`
         + `})();\n`;
         return code;
