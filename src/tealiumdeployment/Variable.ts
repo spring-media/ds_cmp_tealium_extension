a9b46c11d1747cd93add62db41a0e0207b6c28fb
Tobias Steuer <tobias.steuer@axelspringer.com>
2026-02-18 12:49:23 +0100

    [TIPPI-1355] Add notes and uniqueIdentifier to variable

--- src/tealiumdeployment/Variable.ts
+++ src/tealiumdeployment/Variable.ts
@@ -1,7 +1,25 @@
 export class Variable {
+
+    public readonly uniqueIdentifier: string;
+    private notes: string | null;
+
     constructor(
         public readonly id: number, 
-        public readonly name: string) {
+        public readonly name: string,
+        public readonly type: 'udo' | 'cp'
+    ) {
+        if(this.name.length === 0) {
+            throw new Error('Variable name can not be empty string');
+        }
+        this.uniqueIdentifier = `${this.type}.${this.name}`;
+        this.notes = null;
+    }
 
+    setNotes(notes: string | null) {
+        this.notes = notes;
+    }
+
+    getNotes(): string | null {
+        return this.notes;
     }
 }
--- src/tealiumdeployment/Variables.test.ts
+++ src/tealiumdeployment/Variables.test.ts
@@ -3,12 +3,50 @@ import { Variable } from './Variable';
 describe('Variables', () => {
 
     it('has an ID', ()=> {
-        const variable = new Variable(123, 'TestVariable');
+        const variable = new Variable(123, 'TestVariable', 'udo');
         expect(variable.id).toBe(123);
     });
 
     it('has a name', ()=> {
-        const variable = new Variable(123, 'TestVariable');
+        const variable = new Variable(123, 'TestVariable', 'udo');
         expect(variable.name).toBe('TestVariable');
+    })
+
+    it('has a type', ()=> {
+        const variable = new Variable(123, 'TestVariable', 'udo');
+        expect(variable.type).toBe('udo');
+
+        //TODO cp and qp meta
+    })
+
+    describe('create new instance', () => {
+        it('does not allow empty names', () => {
+            expect(()=> new Variable(123, 'test', 'udo')).not.toThrow();
+            expect(()=> new Variable(123, '', 'udo')).toThrow('Variable name can not be empty string');
+        });
+    });
+
+    describe('uniqueIdentifier', () => {
+        it('returns a combination of name and type', () => {
+            const variableA = new Variable(123, 'ad_suite', 'udo');
+            expect(variableA.uniqueIdentifier).toBe('udo.ad_suite');
+
+            const variableB = new Variable(123, 'ad_suite', 'cp');
+            expect(variableB.uniqueIdentifier).toBe('cp.ad_suite');
+        });
+    });
+
+    describe('notes', () => {
+
+        it('has no notes by default', ()=> {
+            const variable = new Variable(123, 'ad_suite', 'udo');
+            expect(variable.getNotes()).toBe(null);
+        });
+
+        it('can have notes', ()=> {
+            const variable = new Variable(123, 'ad_suite', 'udo');
+            variable.setNotes('My test note');
+            expect(variable.getNotes()).toBe('My test note');
+        });
     })
 });
