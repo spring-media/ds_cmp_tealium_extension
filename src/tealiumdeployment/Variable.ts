01691578abcc760f70dbef483440951938c5e270
Tobias Steuer <tobias.steuer@axelspringer.com>
2026-02-18 13:16:49 +0100

    [TIPPI-1355] Add alias, getHash and some variables to welt/variables.ts

--- src/tealiumdeployment/Variable.ts
+++ src/tealiumdeployment/Variable.ts
@@ -1,25 +1,50 @@
+import crypto from 'node:crypto';
+
 export class Variable {
 
     public readonly uniqueIdentifier: string;
     private notes: string | null;
+    private alias: string | null;
 
     constructor(
         public readonly id: number, 
         public readonly name: string,
-        public readonly type: 'udo' | 'cp'
+        public readonly type: 'udo' | 'cp' | 'qp' | 'meta'
     ) {
         if(this.name.length === 0) {
             throw new Error('Variable name can not be empty string');
         }
         this.uniqueIdentifier = `${this.type}.${this.name}`;
         this.notes = null;
+        this.alias = null;
     }
 
     setNotes(notes: string | null) {
         this.notes = notes;
+        return this;
     }
 
     getNotes(): string | null {
         return this.notes;
+    }
+
+    setAlias(alias: string | null) {
+        this.alias = alias;
+        return this;
+    }
+
+    getAlias(): string | null {
+        return this.alias;
+    }
+
+    getHash() {
+            const content = {
+                id: this.id,
+                name: this.name,
+                type: this.type,
+                notes: this.notes,
+                alias: this.alias
+            };
+            return crypto.createHash('sha256').update(JSON.stringify(content)).digest('hex');
     }
 }
--- src/tealiumdeployment/Variables.test.ts
+++ src/tealiumdeployment/Variables.test.ts
@@ -13,10 +13,17 @@ describe('Variables', () => {
     })
 
     it('has a type', ()=> {
-        const variable = new Variable(123, 'TestVariable', 'udo');
-        expect(variable.type).toBe('udo');
+        const variableA = new Variable(123, 'TestVariable', 'udo');
+        expect(variableA.type).toBe('udo');
 
-        //TODO cp and qp meta
+        const variableB = new Variable(123, 'TestVariable', 'cp');
+        expect(variableB.type).toBe('cp');
+
+        const variableC = new Variable(123, 'TestVariable', 'qp');
+        expect(variableC.type).toBe('qp');
+
+        const variableD = new Variable(123, 'TestVariable', 'meta');
+        expect(variableD.type).toBe('meta');
     })
 
     describe('create new instance', () => {
@@ -47,6 +54,64 @@ describe('Variables', () => {
             const variable = new Variable(123, 'ad_suite', 'udo');
             variable.setNotes('My test note');
             expect(variable.getNotes()).toBe('My test note');
+        });
+    });
+
+    describe('alias', () => {
+
+        it('has no alias by default', ()=> {
+            const variable = new Variable(123, 'ad_suite', 'udo');
+            expect(variable.getAlias()).toBe(null);
+        });
+
+        it('can have notes', ()=> {
+            const variable = new Variable(123, 'ad_suite', 'udo');
+            variable.setAlias('MySpecialVariable');
+            expect(variable.getAlias()).toBe('MySpecialVariable');
+        });
+    });
+
+    describe('getHash', () => {
+        it('is based on id, name, type, alias and notes', ()=>{
+            const variable = new Variable(1, 'test', 'udo')
+                .setAlias('Im a variable')
+                .setNotes('Hello World!');
+            expect(variable.getHash()).toBe('1c0b15a816e099b494ba418213c42b1600c2db32ce7f276910673390117154f6');
+        });
+
+        it('changes if id changes', ()=>{
+            const variable = new Variable(2, 'test', 'udo')
+                .setAlias('Im a variable')
+                .setNotes('Hello World!');
+            expect(variable.getHash()).toBe('edd0c355feb79888a7484a02204d21abc4f22c2f95c960f17c4077b1452479c1');
+        });
+
+        it('changes if name changes', ()=>{
+            const variable = new Variable(1, 'test-abc', 'udo')
+                .setAlias('Im a variable')
+                .setNotes('Hello World!');
+            expect(variable.getHash()).toBe('bcc95b10d0afea5cf666deb91440337462bfd1a3261c57626eaae0a646e287ef');
+        });
+
+        it('changes if type changes', ()=>{
+            const variable = new Variable(1, 'test', 'cp')
+                .setAlias('Im a variable')
+                .setNotes('Hello World!');
+            expect(variable.getHash()).toBe('acf702c37b265d68c4fbf39a885091586983ac396475d784ae2eea2f215a2c10');
+        });
+
+        it('changes if alias changes', ()=>{
+            const variable = new Variable(1, 'test', 'udo')
+                .setAlias('Im another variable')
+                .setNotes('Hello World!');
+            expect(variable.getHash()).toBe('947152b133f84ef25696e2f2b0b15533fd74de35e8566f9eb914027329657903');
+        });
+
+        it('changes if notes changes', ()=>{
+            const variable = new Variable(1, 'test', 'udo')
+                .setAlias('Im another variable')
+                .setNotes('Just a test');
+            expect(variable.getHash()).toBe('03125e56800229c3e0ffc8063f9f358d4e2e1b23e7882c21912f48d90cad107d');
         });
     })
 });
